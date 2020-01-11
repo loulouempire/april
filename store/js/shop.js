5d6555fae8d5f2b35dc75efbee8d93678ed34f2c
loulouempire <59313668+loulouempire@users.noreply.github.com>
2020-01-11 23:11:54 +0000

    Add files via upload

---
+++ store/js/shop.js
@@ -0,0 +1,21 @@
+$("#feature-1").hover(
+  function() {
+    $("#feature-1 .tint").addClass('color-black');
+  }
+);
+
+$("#feature-2").hover(
+  function() {
+    $("#feature-2 .tint").addClass('color-black');
+  }
+);
+$("#feature-3").hover(
+  function() {
+    $("#feature-3 .tint").addClass('color-black');
+  }
+);
+$("#feature-4").hover(
+  function() {
+    $("#feature-4 .tint").addClass('color-black');
+  }
+);
